3a2889a35d93a352d2e534d3afee872cf96b4d06
Vasyl <vasyaboychuk@gmail.com>
2022-04-10 23:01:00 +0300

    esbuild

--- infrastructure/ScreenCardsStack.ts
+++ infrastructure/ScreenCardsStack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
 import { GenericTable } from "./GenericTable";
@@ -17,6 +18,12 @@ export class ScreenCardsStack extends Stack {
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: 'hello.main',
     });
+
+    const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+      entry: (join(__dirname, '..', 'services', 'node-lambda', 'hello.ts')),
+      handler: 'handler'
+    });
+
     const helloLambdaIntegration = new LambdaIntegration(helloLambda);
     const helloLambdaResource = this.api.root.addResource('hello');
     helloLambdaResource.addMethod('GET', helloLambdaIntegration);
