e2b3bdd964752eb9fccc532f1fa7e207fd33af5c
Vasyl <vasyaboychuk@gmail.com>
2022-04-17 16:19:40 +0300

    hello .test debug

--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -6,6 +6,5 @@ const event = {
   }
 }
 
-const result = handler(event as any, {} as any);
-console.debug(result);
-console.log('step 3')
+handler(event as any, {} as any)
+  .then(result => console.debug(result));
