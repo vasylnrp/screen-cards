b4b1cf3914e996304f246eb31b7fc7ccf5a8647f
Vasyl <vasyaboychuk@gmail.com>
2022-05-01 13:45:07 +0300

    added delete class

---
+++ services/ScreenCardsTable/Delete.ts
@@ -0,0 +1,35 @@
+import { DynamoDB } from "aws-sdk";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+const TABLE_NAME = process.env.TABLE_NAME!;
+const PRIMARY_KEY = process.env.PRIMARY_KEY;
+const dbClient = new DynamoDB.DocumentClient();
+
+async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+  let result: APIGatewayProxyResult = {
+    body: 'Success',
+    statusCode: 204,
+  };
+
+  const cardId = event.queryStringParameters?.[PRIMARY_KEY!]
+  try {
+    if (cardId) {
+      const dynamoResult = await dbClient.delete({
+        TableName: TABLE_NAME,
+        Key: {
+          'cardId': cardId,
+        },
+      }).promise();
+
+      result.body = JSON.stringify(dynamoResult);
+    }
+  } catch (error: any) {
+    result = {
+      body: error.message,
+      statusCode: 500,
+    }
+  }
+  return result;
+}
+
+export { handler }
--- test/playground/Hello.test.ts
+++ test/playground/Hello.test.ts
@@ -1,13 +1,10 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
-import { handler } from "../../services/ScreenCardsTable/Update"
+import { handler } from "../../services/ScreenCardsTable/Delete"
 
 const event: APIGatewayProxyEvent = {
   queryStringParameters: {
     cardId: 'b842a1b6-35da-4bd3-b2e2-652f3aa3bbfb'
   },
-  body: {
-    location: 'Poruchyn'
-  }
 } as any;
 
 const result = handler(event, {} as any)
