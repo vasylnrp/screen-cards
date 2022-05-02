70c04034ccda110942b5294ffae65fecffc36ac2
Vasyl <vasyaboychuk@gmail.com>
2022-05-02 23:09:21 +0300

    user implify testing

---
+++ test/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  REGION: 'ue-central-1',
+  USER_POOL_ID: 'eu-central-1_PQ1wn6flS',
+  APP_CLIENT_ID: '5kuerqj9ut9tcp04qq2qoj21sm',
+  TEST_USER_NAME: 'vasylpool',
+  TEST_USER_PASSWORD: 'lDjasdf1#@2' // TODO: set correct pass seq
+}
